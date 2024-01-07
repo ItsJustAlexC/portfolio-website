ae42ee7708722a7f3ecc2f5bcdf2fbe1098e2ad5
Alex <alexcampbell1124@gmail.com>
2024-01-07 15:34:23 -0600

    updated hero and about me sections

--- portfolio-website/src/components/About.jsx
+++ portfolio-website/src/components/About.jsx
@@ -1,15 +1,40 @@
+import React, { useEffect, useRef, useState } from 'react';
+
 import profileImage from '../images/profile.png';
 
 const About = () => {
+    const heroContentRef = useRef();
+    const [hasEntered, setHasEntered] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.intersectionRatio >= 0.4) {
+                    entry.target.classList.add('fade-up');
+                    setHasEntered(true);
+                }
+            });
+        }, {threshold : 0.4});
+
+        observer.observe(heroContentRef.current);
+
+        // Clean up on unmount
+        return () => observer.disconnect();
+    }, []);
+
     return (
         <div className="about">
-            <div className="about-content">
+            <div className="about-content" ref={heroContentRef}>
                 <h1 className="about-h1">About Me</h1>
-                <img className="profile" src={profileImage} alt="Alex"/>
+                <img className="profile" src={profileImage} alt="Alex" draggable="false"/>
                 <p className="about-p">
-                Hi! My name's Alex and I love creating things. In college I started out as a music major, until one day I decided to take a course in Java. 
-                            Almost instantly I saw my love of creating music turn into a love of creating applications that other people could use. Fast-forward to today, I now specialize in Java full-stack development.
-                            I have had the privilege of working with a <a className=""href="https://www.genesis10.com">consultant company</a> and I currently work at a <a className=""href="https://www.valleyqueen.com/">major cheese company</a>.
+                    Hi! My name's Alex and I love creating things. In college I started out as a music major, until one day I decided to take a course in Java. 
+                    Almost instantly I saw my love of creating music turn into a love of creating applications that other people could use. Fast-forward to today, I now specialize in Java and C# full-stack development.
+                    I have had the privilege of working with a <a href="https://www.genesis10.com">consultant company</a> and I currently work at a <a href="https://www.valleyqueen.com/">major cheese company</a>.
+                </p>
+                <br/>
+                <p className='about-p'>
+                    While I'm not coding, you can find me at the gym, watching youtube, or on my switch playing roguelikes.
                 </p>
             </div>
         </div>
--- portfolio-website/src/components/Hero.jsx
+++ portfolio-website/src/components/Hero.jsx
@@ -1,14 +1,13 @@
 const Hero = () => {
     return (
-        <div className="hero">
+        <div className="hero hero-bg-img">
             <div className="hero-content">
                 <h1 className="hero-h1 fade-down">Hi! I'm Alex</h1>
-                <h2 className="hero-h2 fade-up">Full-Stack Developer</h2>
-                <span className="hero-span hover"><i className="fa-solid fa-chevron-down"></i></span>
+                <h2 className="hero-h2 fade-up">Full-Stack Developer </h2>
+                <span className="hero-span hover"><i className="fa-solid fa-chevron-down hero-arrow"></i></span>
             </div>
         </div>
     )
 }
-
 
 export default Hero;
