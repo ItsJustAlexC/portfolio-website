e224115109226d897bee5de119146c9637d749b1
Alex <alexcampbell1124@gmail.com>
2024-01-07 01:07:03 -0600

    added hero section

---
+++ portfolio-website/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="hero-content">
+                <h1 className="hero-h1 fade-down">Hi! I'm Alex</h1>
+                <h2 className="hero-h2 fade-up">Full-Stack Developer</h2>
+            </div>
+        </div>
+    )
+}
+
+
+export default Hero;
