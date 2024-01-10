814a1acf2c85bcc2dabcc59c6ba1ac94cde70aa7
Alex <alexcampbell1124@gmail.com>
2024-01-10 17:39:09 -0600

    updated navbar with more sections and removed copyright symbol in footer

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App containter">
+    <div className="App">
       <Navbar/>
       <Hero/>
       <About/>
