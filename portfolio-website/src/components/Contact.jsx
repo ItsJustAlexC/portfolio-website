023084041cb9516eabba986cc29cc9df126e678d
Alex <alexcampbell1124@gmail.com>
2024-01-12 23:43:02 -0600

    added contact section

---
+++ portfolio-website/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+const Contact = () => {
+    return (
+        <div id="contact" className="contact container-fluid">
+            <div className="contact-content">
+                <i className="contact-img fa-regular fa-paper-plane "></i>
+                <h1 className="contact-h1">Get in touch!</h1>
+                <p>I am currently not looking for new opportunities. But you may still email me just to say hi! :)</p>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
